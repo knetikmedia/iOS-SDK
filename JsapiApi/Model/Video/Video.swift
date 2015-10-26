a4dd4bf8ffd9ffe790d3400dab625a85ead8c75d
youssef <youssef>
2015-10-26 15:04:38 +0200

    fix toDictionary function

--- JsapiApi/Model/Video/Video.swift
+++ JsapiApi/Model/Video/Video.swift
@@ -68,7 +68,7 @@ public class Video :NSObject{
 	/**
 	 * Returns all the available property values in the form of NSDictionary object where the key is the approperiate json key and the value is the value of the corresponding property
 	 */
-	func toDictionary() -> NSDictionary
+	public func toDictionary() -> NSDictionary
 	{
 		let dictionary = NSMutableDictionary()
 		if author != nil{
