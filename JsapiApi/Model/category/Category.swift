148936ecead7f03a30ac28f7c2a03505e7661084
youssef <youssef>
2015-11-09 21:55:40 +0200

    Add Category class

---
+++ JsapiApi/Model/category/Category.swift
@@ -0,0 +1,17 @@
+//
+//	Comment.swift
+//
+//	Create by Youssef on 8/4/2015
+//	Copyright © 2015. All rights reserved.
+import Foundation
+
+public class Category:NSObject{
+
+	public var categoryId : String!
+	public var categoryName : NSNumber!
+	public var image : String!
+	
+    override public init(){super.init()}
+
+    
+}
